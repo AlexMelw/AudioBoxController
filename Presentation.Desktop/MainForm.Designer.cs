ac37510ef31b95ae80c0ff7597674a443aa46fa2
Veaceslav BARBARII <slava.barbarii@gmail.com>
2017-04-08 23:36:54 +0300

    Add juicy interface

--- Presentation.Desktop/MainForm.Designer.cs
+++ Presentation.Desktop/MainForm.Designer.cs
@@ -36,11 +36,89 @@ namespace Presentation.Desktop
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            Syncfusion.Windows.Forms.Tools.ActiveStateCollection activeStateCollection1 = new Syncfusion.Windows.Forms.Tools.ActiveStateCollection();
+            Syncfusion.Windows.Forms.Tools.InactiveStateCollection inactiveStateCollection1 = new Syncfusion.Windows.Forms.Tools.InactiveStateCollection();
+            Syncfusion.Windows.Forms.Tools.ToggleButtonRenderer toggleButtonRenderer1 = new Syncfusion.Windows.Forms.Tools.ToggleButtonRenderer();
+            Syncfusion.Windows.Forms.Tools.SliderCollection sliderCollection1 = new Syncfusion.Windows.Forms.Tools.SliderCollection();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.playToggleButton = new Syncfusion.Windows.Forms.Tools.ToggleButton();
+            this.closePictureBox = new System.Windows.Forms.PictureBox();
+            this.loadAudioFileButtonAdv = new Syncfusion.Windows.Forms.ButtonAdv();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.playToggleButton)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.closePictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // playToggleButton
+            // 
+            this.playToggleButton.ActiveState = activeStateCollection1;
+            this.playToggleButton.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.playToggleButton.ForeColor = System.Drawing.Color.Black;
+            inactiveStateCollection1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(151)))), ((int)(((byte)(151)))), ((int)(((byte)(151)))));
+            this.playToggleButton.InactiveState = inactiveStateCollection1;
+            this.playToggleButton.Location = new System.Drawing.Point(734, 12);
+            this.playToggleButton.MinimumSize = new System.Drawing.Size(52, 20);
+            this.playToggleButton.Name = "playToggleButton";
+            this.playToggleButton.Renderer = toggleButtonRenderer1;
+            this.playToggleButton.Size = new System.Drawing.Size(90, 40);
+            this.playToggleButton.Slider = sliderCollection1;
+            this.playToggleButton.TabIndex = 0;
+            this.playToggleButton.Text = "toggleButton1";
+            // 
+            // closePictureBox
+            // 
+            this.closePictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.closePictureBox.Location = new System.Drawing.Point(850, 12);
+            this.closePictureBox.Name = "closePictureBox";
+            this.closePictureBox.Size = new System.Drawing.Size(32, 32);
+            this.closePictureBox.TabIndex = 1;
+            this.closePictureBox.TabStop = false;
+            // 
+            // loadAudioFileButtonAdv
+            // 
+            this.loadAudioFileButtonAdv.BeforeTouchSize = new System.Drawing.Size(75, 23);
+            this.loadAudioFileButtonAdv.IsBackStageButton = false;
+            this.loadAudioFileButtonAdv.Location = new System.Drawing.Point(685, 333);
+            this.loadAudioFileButtonAdv.Name = "loadAudioFileButtonAdv";
+            this.loadAudioFileButtonAdv.Size = new System.Drawing.Size(75, 23);
+            this.loadAudioFileButtonAdv.TabIndex = 2;
+            this.loadAudioFileButtonAdv.Text = "buttonAdv1";
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(12, 12);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(385, 169);
+            this.axWindowsMediaPlayer1.TabIndex = 3;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(894, 549);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.loadAudioFileButtonAdv);
+            this.Controls.Add(this.closePictureBox);
+            this.Controls.Add(this.playToggleButton);
+            this.Name = "MainForm";
             this.Text = "MainForm";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.playToggleButton)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.closePictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Syncfusion.Windows.Forms.Tools.ToggleButton playToggleButton;
+        private System.Windows.Forms.PictureBox closePictureBox;
+        private Syncfusion.Windows.Forms.ButtonAdv loadAudioFileButtonAdv;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
     }
 }
 
--- Presentation.Desktop/MainForm.cs
+++ Presentation.Desktop/MainForm.cs
@@ -1,26 +1,134 @@
-#region Copyright Syncfusion Inc. 2001 - 2017
-// Copyright Syncfusion Inc. 2001 - 2017. All rights reserved.
-// Use of this code is subject to the terms of our license.
-// A copy of the current license can be obtained at any time by e-mailing
-// licensing@syncfusion.com. Any infringement will be prosecuted under
-// applicable laws. 
-#endregion
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
+using System.IO;
+using System.Media;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
+using Presentation.Desktop.Properties;
+using Syncfusion.Windows.Forms.Tools;
 
 namespace Presentation.Desktop
 {
     public partial class MainForm : Form
     {
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+        private SoundPlayer _soundPlayer;
+        private string _audioFilePath;
+
+        #region CONSTRUCTORS
+
         public MainForm()
         {
             InitializeComponent();
+            _soundPlayer = new SoundPlayer();
+        }
+
+        #endregion
+
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            SetControlsProperties();
+            RegisterControlsEvents();
+        }
+
+        private void RegisterControlsEvents()
+        {
+            #region Menu Buttons
+
+            loadAudioFileButtonAdv.Click += (sender, args) =>
+            {
+                using (OpenFileDialog openFileDialog = new OpenFileDialog())
+                {
+                    openFileDialog.Filter = Resources.FileFilters;
+
+                    if (openFileDialog.ShowDialog() == DialogResult.OK) // Test result.
+                    {
+                        _audioFilePath = openFileDialog.FileName;
+                        try
+                        {
+                            if (!_audioFilePath.EndsWith(@".wav", StringComparison.OrdinalIgnoreCase)) { }
+                            else
+                            {
+                                _soundPlayer = new SoundPlayer(_audioFilePath);
+                            }
+                        }
+                        catch (IOException exception)
+                        {
+                            Console.WriteLine(exception.StackTrace);
+                            throw;
+                        }
+                    }
+                }
+            };
+
+            #endregion
+
+            playToggleButton.ToggleStateChanged += (sender, args) =>
+            {
+                if (_soundPlayer == null)
+                    return;
+
+                if (playToggleButton.ToggleState == ToggleButtonState.Active)
+                {
+                    _soundPlayer.Play();
+                    return;
+                }
+                else
+                    _soundPlayer.Stop(); // ToggleButtonState.Inactive
+            };
+
+            this.MouseDown += (sender, mouseEventArgs) =>
+            {
+                if (mouseEventArgs.Button == MouseButtons.Left)
+                {
+                    ReleaseCapture();
+                    SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+                }
+            };
+
+            closePictureBox.Click += (sender, args) => this.Close();
+        }
+
+        private void SetControlsProperties()
+        {
+            #region Play Toggle Button
+
+            playToggleButton.ActiveState.BackColor = playToggleButton.InactiveState.BackColor;
+            playToggleButton.ActiveState.HoverColor = playToggleButton.InactiveState.HoverColor;
+            playToggleButton.ActiveState.ForeColor = playToggleButton.InactiveState.ForeColor;
+            playToggleButton.ActiveState.Text = @"STOP   ";
+            playToggleButton.InactiveState.Text = @"PLAY   ";
+
+            #endregion
+
+            #region MainForm
+
+            this.BackColor = Color.FromArgb(226, 226, 226);
+            this.FormBorderStyle = FormBorderStyle.None;
+
+            #endregion
+
+            #region Close Button
+
+            closePictureBox.BackgroundImage = Image.FromFile(@"Resources\close-icon.png");
+            closePictureBox.SizeMode = PictureBoxSizeMode.AutoSize;
+            closePictureBox.Size = new Size(32, 32);
+            closePictureBox.Margin = new Padding(0, 0, 0, 0);
+
+            #endregion
+
+            #region Load Audio File
+
+            loadAudioFileButtonAdv.Text = @"Load";
+
+            #endregion
         }
     }
 }
